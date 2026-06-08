24ef9b54a8d472925db291b1078332e2eee0307b
Ram Manohar <81113789+brightBUM@users.noreply.github.com>
2026-06-08 12:23:59 +0530

    shaders project

--- Shaders/Resources/Shaders/default.frag
+++ Shaders/Resources/Shaders/default.frag
@@ -1,11 +1,15 @@
 #version 460 core
 out vec4 FragColor;
 in vec3 Color;
-in vec2 Texcoord;
 uniform float time; // uniform - special keyword to receive data from cpu
-uniform sampler2D texSampler;
+
 void main()
 {
-      vec4 texValue = texture(texSampler,Texcoord);
-      FragColor = vec4(texValue);
+//      FragColor = vec4(Color, 1.0f);
+//    FragColor = vec4(Color.x,Color.y,Color.z, 1.0f);
+    FragColor = vec4(Color*sin(time), 1.0f);
+//
+
+//    FragColor = vec4(Color.xy,Color.z,1.0f);  //swizzling
+//    FragColor = vec4(Color.rgb,1.0f);
 } 
--- Shaders/Source.cpp
+++ Shaders/Source.cpp
@@ -2,15 +2,12 @@
 #include"glad/glad.h"
 #include"GLFW/glfw3.h"
 #include"Shader.h"
-#define STB_IMAGE_IMPLEMENTATION
-#include"stb_image.h"
+
 #define HEIGHT 800
 #define WIDTH 600
 #define Log(x) std::cout<<x<<std::endl;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-
-void LoadTexture(unsigned int& texture,const char* fileName);
 
 int main()
 {
@@ -36,22 +33,20 @@ int main()
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
-
-	stbi_set_flip_vertically_on_load(true);
 #pragma endregion
 
-#pragma region Vertex Buffer & Texture
+#pragma region Vertex Buffer
 
-	float vertices[] = 
+	float vertices[] =
 	{
-		//pos                //col             //tex coords
-		-0.5f, -0.5f, 0.0f,	 1.0f, 0.0f, 0.0f, 0.0f,0.0f, // point -1
-		 0.5f, -0.5f, 0.0f,	 0.0f, 1.0f, 0.0f, 1.0f,0.0f, // point -2
-		 0.0f,  0.5f, 0.0f,	 0.0f, 0.0f, 1.0f, 0.5f,1.0f  // point -3
+		//pos                //col
+		-0.5f, -0.5f, 0.0f,	 1.0f, 0.0f, 0.0f, // point -1
+		 0.5f, -0.5f, 0.0f,	 0.0f, 1.0f, 0.0f, // point -2
+		 0.0f,  0.5f, 0.0f,	 0.0f, 0.0f, 1.0f  // point -3
 	};
 	//VBO - vertex buffer object
 	//VAO - vertex array object
-	unsigned int VBO,VAO;
+	unsigned int VBO, VAO;
 	glGenBuffers(1, &VBO);
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
@@ -60,11 +55,6 @@ int main()
 	//assign vertex data to buffer
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-
-	unsigned int texture_1,texture_2;
-	LoadTexture(texture_1, "Resources/Textures/test.jpeg");
-	LoadTexture(texture_2, "Resources/Textures/raman.png");
-	
 #pragma endregion
 
 #pragma region Shaders
@@ -74,20 +64,17 @@ int main()
 	//2nd - para - size of the component;
 	//5th - para - total size of the vertex/point
 	//6th - para - offset within the vertex size
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3*sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8* sizeof(float), (void*)(6 * sizeof(float)));
-	glEnableVertexAttribArray(2);
 	Shader defaultShader("Resources/Shaders/default.vert",
-						 "Resources/Shaders/default.frag");
+		"Resources/Shaders/default.frag");
 
 	defaultShader.use();
-	defaultShader.SetInt("texSampler_1", 0);
-	defaultShader.SetInt("texSampler_2", 1);
+
 #pragma endregion
 
 #pragma region RenderLoop
@@ -104,17 +91,10 @@ int main()
 		//logic
 		defaultShader.use();
 		defaultShader.SetFloat("time", glfwGetTime());
-		//Log(glfwGetTime());
-
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, texture_1);
-
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, texture_2);
 
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
-		
+
 		//Log(glfwGetTime());
 
 		/* Swap front and back buffers */
@@ -131,37 +111,6 @@ int main()
 	return 0;
 #pragma endregion
 
-}
-void LoadTexture(unsigned int& texture,const char* fileName)
-{
-	glGenTextures(1, &texture);
-
-	glBindTexture(GL_TEXTURE_2D, texture);
-	int width, height, nrChannels;
-	unsigned char* data = stbi_load(fileName, &width, &height, &nrChannels, 0);
-	Log(nrChannels);
-	if (data)
-	{
-		switch (nrChannels)
-		{
-		case 3: // jpg format
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-			break;
-		case 4:// png format
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-			break;
-		default:
-			Log("invalid image channels")
-			break;
-		}
-		
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-	{
-		std::cout << "Failed to load texture" << std::endl;
-	}
-	stbi_image_free(data);
 }
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
