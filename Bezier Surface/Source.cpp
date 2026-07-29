f16938fb6a4929dd5600a91674616885f5b83da6
Ram Manohar <81113789+brightBUM@users.noreply.github.com>
2026-07-29 15:58:50 +0530

    bezier wireframe

--- Bezier Surface/Source.cpp
+++ Bezier Surface/Source.cpp
@@ -15,7 +15,9 @@ float worldX, worldY;
 double prevX, prevY;
 
 std::vector<glm::vec3> points;
-std::vector<glm::vec3> BezierPoints;
+std::vector<glm::vec3> sampleRowBezierPoints;
+std::vector<glm::vec3> rowBezierPoints;
+std::vector<glm::vec3> columnBezierPoints;
 
 float t = 0.0f;
 int segments = 10;
@@ -252,7 +254,7 @@ int main()
 
 	Log("points size : "<<points.size());
 
-	unsigned int VBO, VAO, lerpVAO, lerpVBO;
+	unsigned int VBO, VAO, columnVAO, columnVBO,rowVAO,rowVBO;
 	glGenBuffers(1, &VBO);
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
@@ -274,26 +276,64 @@ int main()
 		{
 			rowPoints.push_back(points[j]);
 		}
-
-		for (int k = 0;k <= segments;k++)
+		float u;
+		for (int k = 0;k <= segments;k++)  
 		{
-			t = k / static_cast<float>(segments);
+			u = k / static_cast<float>(segments);
 
-			auto point = BezierPoint(rowPoints, t);
+			auto point = BezierPoint(rowPoints, u);
 
-			BezierPoints.push_back(point);
+			sampleRowBezierPoints.push_back(point);
 		}
 
 	}
-	
 
-	Log("bezierPoints size : " << BezierPoints.size());
+	for (int i = 0;i < segments + 1;i++)
+	{
+		std::vector<glm::vec3> columnPoints;
+		for (int j = i;j < sampleRowBezierPoints.size();j+=segments+1)
+		{
+			auto point = sampleRowBezierPoints[j];
+			columnPoints.push_back(point);
+		}
+		float v;
+		for (int k = 0;k <= segments;k++)
+		{
+			v = k / static_cast<float>(segments);
 
-	glGenBuffers(1, &lerpVBO);
-	glGenVertexArrays(1, &lerpVAO);
-	glBindVertexArray(lerpVAO);
-	glBindBuffer(GL_ARRAY_BUFFER, lerpVBO);
-	glBufferData(GL_ARRAY_BUFFER, BezierPoints.size() * sizeof(glm::vec3), BezierPoints.data(), GL_STATIC_DRAW);
+			auto point = BezierPoint(columnPoints, v);
+			//Log(" columnBezierPoints : " << point.x << " , " << point.y << " , " << point.z);
+
+			columnBezierPoints.push_back(point);
+		}
+	}
+
+	for (int i = 0;i < segments + 1;i++)
+	{
+		for (int j = i ;j < columnBezierPoints.size();j+=segments+1)
+		{
+			rowBezierPoints.push_back(columnBezierPoints[j]);
+		}
+	}
+
+	Log("sample row bezierPoints size : " << sampleRowBezierPoints.size());
+	Log("column bezierPoints size : " << columnBezierPoints.size());
+	Log("row bezierPoints size : " << rowBezierPoints.size());
+
+	glGenBuffers(1, &columnVBO);
+	glGenVertexArrays(1, &columnVAO);
+	glBindVertexArray(columnVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, columnVBO);
+	glBufferData(GL_ARRAY_BUFFER, columnBezierPoints.size() * sizeof(glm::vec3), columnBezierPoints.data(), GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glGenBuffers(1, &rowVBO);
+	glGenVertexArrays(1, &rowVAO);
+	glBindVertexArray(rowVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, rowVBO);
+	glBufferData(GL_ARRAY_BUFFER, columnBezierPoints.size() * sizeof(glm::vec3), columnBezierPoints.data(), GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -331,62 +371,98 @@ int main()
 			glm::vec3 hitPoint;
 			if (selectedPoint == nullptr)
 			{
-				if (RayPlaneIntersection(camera.Position, mouseRay, glm::vec3(0.0f, 0.0f, 0.0f),
-					glm::vec3(0.0f, 1.0f, 0.0f), t, hitPoint))
+				for (int i = 0;i < points.size();i++)
 				{
-					for (int i = 0;i < points.size();i++)
+					RayPlaneIntersection(camera.Position, mouseRay, points[i],
+						glm::vec3(0.0f, 1.0f, 0.0f), t, hitPoint);
+
+					if (glm::distance(points[i], hitPoint) <= 0.2f)
 					{
-						if (glm::distance(points[i], hitPoint) <= 0.2f)
-						{
-							selectedPoint = &points[i];
-							*selectedPoint = hitPoint;
+						selectedPoint = &points[i];
+						*selectedPoint = hitPoint;
 
-							/*Log("hit point select " << " : "
-								<< hitPoint.x << ", "
-								<< hitPoint.y << ", "
-								<< hitPoint.z);*/
-						}
-
+						/*Log("hit point select " << " : "
+							<< hitPoint.x << ", "
+							<< hitPoint.y << ", "
+							<< hitPoint.z);*/
 					}
+
 				}
 				
 			}
 			else
 			{
-				RayPlaneIntersection(camera.Position, mouseRay, *selectedPoint,
-					glm::vec3(0.0f, 1.0f, 0.0f), t, hitPoint);
+				if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+				{
+					RayPlaneIntersection(camera.Position, mouseRay, *selectedPoint,
+						glm::vec3(0.0f, 0.0f, 1.0f), t, hitPoint);
+				}
+				else
+				{
+					RayPlaneIntersection(camera.Position, mouseRay, *selectedPoint,
+						glm::vec3(0.0f, 1.0f, 0.0f), t, hitPoint);
+				}
 
-				hitPoint.y = 0.0f;
+				//hitPoint.y = 0.0f;
 				*selectedPoint = hitPoint;
-				/*Log("hit point drag " << " : "
-					<< hitPoint.x << ", "
-					<< hitPoint.y << ", "
-					<< hitPoint.z);*/
+				
 			}
 
 		}
 
-		//Log("selected Point : " << selectedPoint);
-	
-		for (int i = 0;i < points.size();i += 3)
+		//update sample rows
+		for (int row = 0; row < 3; row++)
 		{
 			std::vector<glm::vec3> rowPoints;
-			for (int j = i;j < i + 3;j++)
+
+			for (int col = 0; col < 3; col++)
 			{
-				rowPoints.push_back(points[j]);
+				rowPoints.push_back(points[row * 3 + col]);
 			}
 
-			int l = (i/3) * (segments+1);
-			
-			for (int k = l;k < l+ (segments + 1);k++)
+			for (int sample = 0; sample <= segments; sample++)
 			{
-				t = ((segments + 1) - (l+ (segments + 1) -k))/ static_cast<float>(segments);
-				auto point = BezierPoint(rowPoints, t);
+				float u = sample / static_cast<float>(segments);
 
-				BezierPoints[k] = point;
+				glm::vec3 point = BezierPoint(rowPoints, u);
+
+				int index = row * (segments + 1) + sample;
+
+				sampleRowBezierPoints[index] = point;
 			}
-			Log("\n\n");
+		}
+		//update column 
+		for (int column = 0; column <= segments; column++)
+		{
+			std::vector<glm::vec3> columnPoints;
 
+			for (int row = 0; row < 3; row++)
+			{
+				int index = row * (segments + 1) + column;
+
+				columnPoints.push_back(sampleRowBezierPoints[index]);
+			}
+
+			for (int sample = 0; sample <= segments; sample++)
+			{
+				float v = sample / static_cast<float>(segments);
+
+				glm::vec3 point = BezierPoint(columnPoints, v);
+
+				int index = column * (segments + 1) + sample;
+
+				columnBezierPoints[index] = point;
+			}
+		}
+		//update main rows
+		rowBezierPoints.clear();
+
+		for (int row = 0; row <= segments; row++)
+		{
+			for (int column = row; column < columnBezierPoints.size(); column += segments + 1)
+			{
+				rowBezierPoints.push_back(columnBezierPoints[column]);
+			}
 		}
 
 		glm::mat4 model = glm::mat4(1.0f);
@@ -409,8 +485,8 @@ int main()
 		//control points
 		for (int i = 0;i < points.size();i+=3)
 		{
-			curveShader.SetVec3("objectColor", glm::vec3(0.0f));
-			glDrawArrays(GL_LINE_STRIP, i, 3);
+			/*curveShader.SetVec3("objectColor", glm::vec3(0.0f));
+			glDrawArrays(GL_LINE_STRIP, i, 3);*/
 
 			curveShader.SetVec3("objectColor", glm::vec3(1.0f));
 			glDrawArrays(GL_POINTS, i, 3);
@@ -424,19 +500,45 @@ int main()
 		glPointSize(5.0f);
 		glLineWidth(5.0f);
 
-		glBindVertexArray(lerpVAO);
+		glBindVertexArray(columnVAO);
 
-		glBindBuffer(GL_ARRAY_BUFFER, lerpVBO);
-		glBufferData(GL_ARRAY_BUFFER, BezierPoints.size() * sizeof(glm::vec3), BezierPoints.data(), GL_STATIC_DRAW);
+		glBindBuffer(GL_ARRAY_BUFFER, columnVBO);
+		glBufferData(GL_ARRAY_BUFFER, columnBezierPoints.size() * sizeof(glm::vec3), columnBezierPoints.data(), GL_STATIC_DRAW);
 		
-		for (int i = 0;i < BezierPoints.size() ;i += segments + 1)
+		/*for (int i = 0;i < rowBezierPoints.size() ;i += segments + 1)
 		{
 			curveShader.SetVec3("objectColor", glm::vec3(1.0f, 0.5f, 0.0f));
 			glDrawArrays(GL_LINE_STRIP, i, 11);
 
 			curveShader.SetVec3("objectColor", glm::vec3(0.0f));
 			glDrawArrays(GL_POINTS, i, 11);
+		}*/
+		 
+		curveShader.SetVec3("objectColor", glm::vec3(0.0f));
+		glDrawArrays(GL_POINTS, 0, columnBezierPoints.size());
+		
+
+		//columns bezier curves
+		for (int i = 0;i < columnBezierPoints.size();i += segments + 1)
+		{
+			curveShader.SetVec3("objectColor", glm::vec3(1.0f, 0.5f, 0.0f));
+			glDrawArrays(GL_LINE_STRIP, i, 11);
 		}
+
+		//row bezier curves
+
+		glBindVertexArray(rowVAO);
+
+		glBindBuffer(GL_ARRAY_BUFFER, rowVBO);
+		glBufferData(GL_ARRAY_BUFFER, rowBezierPoints.size() * sizeof(glm::vec3), rowBezierPoints.data(), GL_STATIC_DRAW);
+
+		for (int i = 0;i < rowBezierPoints.size();i += segments + 1)
+		{
+			curveShader.SetVec3("objectColor", glm::vec3(1.0f, 0.5f, 0.0f));
+			glDrawArrays(GL_LINE_STRIP, i, 11);
+		}
+
+		
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
